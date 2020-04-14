e6f6543f68c57cd1f91a6f7bc6422aa2b3a3914b
radoslaw.wojakowski <radoslaw.wojakowski@gmail.com>
2020-04-14 16:23:34 +0200

    Reset Repository

--- SQLgenerated/NotesTableScript2.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Notes] (
-    [Id]      INT            NOT NULL,
-    [Creator] NVARCHAR (MAX) NOT NULL,
-	[Title] NVARCHAR (MAX) NOT NULL,
-    [Content] NVARCHAR (MAX) NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
