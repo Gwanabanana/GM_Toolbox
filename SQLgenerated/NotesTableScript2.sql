80c1e769672226dcfb71a85bf91fb9b78dad5a67
radoslaw.wojakowski <radoslaw.wojakowski@gmail.com>
2020-04-14 16:12:50 +0200

    Create Website

---
+++ SQLgenerated/NotesTableScript2.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Notes] (
+    [Id]      INT            NOT NULL,
+    [Creator] NVARCHAR (MAX) NOT NULL,
+	[Title] NVARCHAR (MAX) NOT NULL,
+    [Content] NVARCHAR (MAX) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
